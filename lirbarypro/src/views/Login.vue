846b69b4de251f0762e88cf9878143883276957d
Louisekay <992139098@qq.com>
2020-11-17 10:39:56 +0800

    前端框架

---
+++ lirbarypro/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+   <el-button type="success">成功按钮</el-button>
+   
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'Login'
+}
+</script>
+
+<style >
+
+</style>
